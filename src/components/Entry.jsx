2a98b49368b85d3120ac84f5c02211860d89186b
Darko Glodaic <darko.glodjaic@gmail.com>
2025-01-24 14:59:08 +0100

    Changed how entry is passed as a prop, updated data

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,17 +1,15 @@
 export default function Entry(props) {
-    // Destructure the img object
-    // const { src, alt } = props.img || {src: "test.jpg", alt: "test"};  // default value in case img is not defined
     return (
         <article className="entry">
-            <img className="entry--image" src={props.img.src} alt={props.img.alt}></img>
+            <img className="entry--image" src={props.entry.img.src} alt={props.entry.img.alt}></img>
             <div className="entry--div">
                 <img className="entry--marker" src="src/images/marker.png" alt="marker icon"></img>
-                <span className="entry--location">{props.country}</span>
-                <a className="entry--link" href={props.googleMapsLink}>View on Google Maps</a>
+                <span className="entry--location">{props.entry.country}</span>
+                <a className="entry--link" href={props.entry.googleMapsLink}>View on Google Maps</a>
             
-                <h2 className="entry--title">{props.title}</h2>
-                <p className="entry--date">{props.date}</p>
-                <p className="entry--description">{props.text}</p>
+                <h2 className="entry--title">{props.entry.title}</h2>
+                <p className="entry--date">{props.entry.date}</p>
+                <p className="entry--description">{props.entry.text}</p>
             </div>
             
         </article>  
--- src/data.js
+++ src/data.js
@@ -2,36 +2,60 @@ export default [
     {
         id: 1,
         img: {
+            src: "/src/images/rome.jpg",
+            alt: "Rome"
+        },
+        title: "Rome",
+        country: "Italy",
+        googleMapsLink: "https://www.google.com/maps/search/?api=1&query=geirangerfjord+norway",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "Rome is the capital city of Italy, located in the central-western part of the country. It is home to the iconic Colosseum and boasts a rich history, with ancient ruins and cultural landmarks throughout the city."
+    },
+    {
+        id: 2,
+        img: {
             src: "/src/images/japan.jpg",
             alt: "Mount Fuji"
         },
         title: "Mount Fuji",
         country: "Japan",
-        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        googleMapsLink: "https://www.google.com/maps/search/?api=1&query=mount+fuji+japan",
         dates: "12 Jan, 2021 - 24 Jan, 2021",
         text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
     },
     {
-        id: 2,
+        id: 3,
+        img: {
+            src: "/src/images/amsterdam.jpg",
+            alt: "Amsterdam"
+        },
+        title: "Amsterdam",
+        country: "Netherlands",
+        googleMapsLink: "https://www.google.com/maps/search/?api=1&query=amsterdam+netherlands",
+        dates: "20 June, 2024 - 26 June, 2024",
+        text: "Amsterdam is the capital city of the Netherlands, located in the western part of the country. It lies along the Amstel River and is known for its picturesque canals, historic buildings, and vibrant cultural scene."
+    },
+    {
+        id: 4,
         img: {
             src: "/src/images/sydney.jpg",
             alt: "Sydney Opera House"
         },
         title: "Sydney Opera House",
         country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        googleMapsLink: "https://www.google.com/maps/search/?api=1&query=sydney+opera+house",
         dates: "27 May, 2021 - 8 Jun, 2021",
         text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
     },
     {
-        id: 3,
+        id: 5,
         img: {
             src: "/src/images/norway.jpg",
             alt: "Geirangerfjord"
         },
         title: "Geirangerfjord",
         country: "Norway",
-        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        googleMapsLink: "https://www.google.com/maps/search/?api=1&query=geirangerfjord+norway",
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
